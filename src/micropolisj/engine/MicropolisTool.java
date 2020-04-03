d8d2d3968897b735a7ae232ff2c18731b42711d2
Tanvi H <tharkare@andrew.cmu.edu>
2020-04-02 21:55:35 -0400

    Tool Colors
    Gave the new_building tool a outline and background color

--- src/micropolisj/engine/MicropolisTool.java
+++ src/micropolisj/engine/MicropolisTool.java
@@ -29,7 +29,7 @@ public enum MicropolisTool
 	POWERPLANT(4, 3000),
 	NUCLEAR(4, 5000),
 	AIRPORT(6, 10000),
-	NEW_BUILDING(3, 8000), //Placeholder for a new tool. 
+	NEW_BUILDING(3, 8000), //NEW ENERGY PLANT FOR FEATURE
 	QUERY(1, 0);
 
 	int size;
